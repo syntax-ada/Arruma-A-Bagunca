5fa279d5f85ce9af7a5e306b72a6d5f7307d8f0e
Pimentel06 <pimentelg333@gmail.com>
2026-09-07 19:52:19 -0300

    Ajustes visuais no menu e perfil do jogador

--- js/menu.js
+++ js/menu.js
@@ -2,14 +2,24 @@ const botaoJogar = document.querySelector("#botao-jogar");
 
 const telaInicial = document.querySelector("#tela-inicial");
 
-const telaFases = document.querySelector("#tela-fases");
+const menuFases = document.querySelector("#menu-fases");
+
+const controlesIniciais = document.querySelector("#controles-iniciais");
+
+const botaoVoltar = document.querySelector("#btn-voltar");
 
 const botaoFase1 = document.querySelector("#botao-fase1");
 
 botaoJogar.addEventListener("click", function () {
   telaInicial.classList.add("escondido");
+  controlesIniciais.classList.add("escondido");
+  menuFases.classList.remove("escondido");
+});
 
-  telaFases.classList.remove("escondido");
+botaoVoltar.addEventListener("click", function () {
+  menuFases.classList.add("escondido");
+  telaInicial.classList.remove("escondido");
+  controlesIniciais.classList.remove("escondido");
 });
 
 botaoFase1.addEventListener("click", function () {
